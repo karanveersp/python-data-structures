b2abfa676074f1cb2822e456c94e4fc889e329a4
Karanveer <karan_4496@hotmail.com>
2020-02-15 19:36:35 -0500

    Added linked list insertion sort methods

--- structs/linked_list.py
+++ structs/linked_list.py
@@ -17,6 +17,35 @@ class LinkedList:
     def __len__(self):
         return self._len
 
+    def _find_insertion_position(self, data):
+        a = None
+        b = self.head
+        while b is not None and data > b.data:
+            a = b
+            b = b.next
+        return a
+
+    def insertion_sort(self):
+        before_current = self.head
+        current = self.head.next
+
+        while current:
+            next_node = current.next  # store before updated on current removal
+            position = self._find_insertion_position(current.data)
+
+            if position == before_current:
+                # means current is already after the proper element
+                before_current = current
+            else:
+                self.remove_after(before_current)
+                if position is None:
+                    # current is smallest
+                    self.prepend(current)
+                else:
+                    self.insert_after(position, current)
+            # advance to next - the preserved original pointer to next for current
+            current = next_node
+
     def append(self, new_node):
         if self.head is None:
             self.head = new_node
@@ -101,6 +130,29 @@ class DoublyLinkedList:
         self.head = None
         self.tail = None
 
+    def insertion_sort(self):
+        current = self.head.next
+        while current:
+            search_node = current.prev
+
+            while search_node and search_node.data > current.data:
+                search_node = search_node.prev
+
+            # remove and re-insert current
+            self.remove(current)
+
+            if search_node is None:
+                # its the smallest element in the list - becomes head
+                current.prev = None
+                self.prepend(current)
+            else:
+                # search_node's data is less than current.data so
+                # current should succeed it
+                self.insert_after(search_node, current)
+
+            # advance to next node
+            current = current.next
+
     def traverse(self):
         node = self.head
         while node:
@@ -129,6 +181,10 @@ class DoublyLinkedList:
         else:
             self.head = new_node
             self.tail = new_node
+
+    def append_all(self, *args):
+        for node in args:
+            self.append(node)
 
     def prepend(self, new_node):
         if self.head:
@@ -178,11 +234,19 @@ class DoublyLinkedList:
 
 
 if __name__ == "__main__":
-    word_list = LinkedList()
-    a = Node("Python")
-    b = Node("Is")
-    c = Node("Fun!")
+    # word_list = LinkedList()
+    # a = Node("Python")
+    # b = Node("Is")
+    # c = Node("Fun!")
+    #
+    # word_list.append_all(a, b, c)
+    #
+    # print(word_list)
+    num_list = LinkedList()
+    num_list.append_all(Node(72), Node(91), Node(53), Node(12))
 
-    word_list.append_all(a, b, c)
+    print("unsorted", num_list)
 
-    print(word_list)
+    num_list.insertion_sort()
+
+    print("sorted", num_list)
